17a400ddc2c50c6acfccfaf28c85bc6133480760
HXM <2602137886@qq.com>
2022-01-28 10:00:29 +0800

    dd

---
+++ src/main/java/com/hxm/bean/Area.java
@@ -0,0 +1,11 @@
+package com.hxm.bean;
+
+import lombok.Data;
+
+@Data
+public class Area {
+    private String code;
+    private String name;
+    private String cityCode;
+    private String provinceCode;
+}
